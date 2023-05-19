22e0775898cd4caabdfc22c764f05effc435bafc
soth-amity <130268293+soth-amity@users.noreply.github.com>
2023-05-19 14:31:17 +0000

    [Add] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,28 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(list_params)
+    #savaでデータ保存している
     book.save
-    redirect_to '/top'
+    redirect_to '/books'
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(list_params)
+    redirect_to book_path(book.id)
   end
 
    private
